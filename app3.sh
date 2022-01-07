b1c0dd36db26c59b00443b935b6d498d8abea60a
Yusuf <61632416+nxjosephofficial@users.noreply.github.com>
2022-01-07 15:21:44 +0300

    Login check (user password)

---
+++ app3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Type your Login Information
+read -p 'Username: ' user
+read -sp 'Password: ' pass
+
+# Check the username and password are valid or not
+if (( $user == "admin" && $pass == "12345" ))
+# give a blank line
+echo
+then
+    echo -e "\nSuccesful login"
+else
+    echo -e "\nUnsuccessful login"
+fi
